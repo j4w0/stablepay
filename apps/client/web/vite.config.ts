94ef2a1db96ada279d5f1e7d735fbeaa49e6547e
Jerry Wong <yuki.n@tuta.io>
2026-02-26 20:44:41 +0900

    try again

--- apps/client/web/vite.config.ts
+++ apps/client/web/vite.config.ts
@@ -12,9 +12,4 @@ export default defineConfig({
     tailwindcss(),
     react(),
   ],
-  build: {
-    rollupOptions: {
-      external: ['@stablepay/common', '@stablepay/client-ui'],
-    },
-  },
 });
