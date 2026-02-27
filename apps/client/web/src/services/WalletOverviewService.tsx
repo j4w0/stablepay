1967562abed01fd39ee28f959b17cf156840fb6a
Jerry Wong <yuki.n@tuta.io>
2026-02-27 11:15:10 +0900

    update config

--- apps/client/web/src/services/WalletOverviewService.tsx
+++ apps/client/web/src/services/WalletOverviewService.tsx
@@ -35,7 +35,11 @@ export const WalletOverviewImpl: React.FC = () => {
   const refreshIntervalMs = 60 * 1000;
   const shouldFetch = Boolean(address && webAuthnKey && status === 'connected');
 
-  const { data: balanceData, isFetching, refetch } = useReadContracts({
+  const {
+    data: balanceData,
+    isFetching,
+    refetch,
+  } = useReadContracts({
     contracts: allTokens.map((token) => ({
       address: token.contractAddress,
       abi: erc20Abi,
